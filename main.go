eb5ddedbd60e09ec8e226d3e0c11cace86723773
kawakami <kawakami.ozone@gmail.com>
2019-02-13 06:26:58 +0000

    Added a simple chunk loader.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -24,63 +25,214 @@ const (
 	idLine = "Line"
 	idStrT = "StrT"
 	idAttr = "Attr"
+	idCInf = "CInf"
+	idDbgi = "Dbgi"
+	idDocs = "Docs"
+	idExDp = "ExDp"
 )
 
-func calcChunkRest(length uint32) int {
+type Chunk struct {
+	Id     string
+	Length uint32
+	Data   []byte
+}
+
+func (this *Chunk) load(buffer *bytes.Buffer, id string) {
+	this.Id = id
+	this.Length = binary.BigEndian.Uint32(buffer.Next(4))
+	this.Data = buffer.Next(int(this.Length))
+
+	buffer.Next(this.calRest())
+}
+
+func (this *Chunk) calRest() int {
 	align := 4
-	return align*((int(length)+align-1)/align) - int(length)
+	length := int(this.Length)
+	return align*((length+align-1)/align) - length
 }
 
 type AtomChunk struct {
-	Id        string
-	Length    uint32
+	Chunk
+
 	AtomCount uint32
 	Labels    []string
 }
 
-func ParseAtom(buffer *bytes.Buffer, id string) *AtomChunk {
+func LoadAtom(buffer *bytes.Buffer, id string) *AtomChunk {
 	chunk := &AtomChunk{}
-	//chunk.Id = string(buffer.Next(4))
-	chunk.Id = id
-	chunk.Length = binary.BigEndian.Uint32(buffer.Next(4))
-	chunk.AtomCount = binary.BigEndian.Uint32(buffer.Next(4))
+	chunk.load(buffer, id)
 
-	length := 12
-
+	data := bytes.NewBuffer(chunk.Data)
+	chunk.AtomCount = binary.BigEndian.Uint32(data.Next(4))
 	labels := []string{}
 	for i := 0; i < int(chunk.AtomCount); i++ {
-		s := int(buffer.Next(1)[0])
-		labels = append(labels, string(buffer.Next(s)))
-		length += 1 + s
+		s := int(data.Next(1)[0])
+		labels = append(labels, string(data.Next(s)))
 	}
 	chunk.Labels = labels
 
-	buffer.Next(calcChunkRest(chunk.Length))
 	return chunk
 }
 
-type Term struct {
-}
-
 type CodeChunk struct {
-	Id         string
-	Length     uint32
+	Chunk
+
 	Version    uint32
 	MaxOpcode  uint32
 	LabelCount uint32
 	FunCount   uint32
 }
 
-func ParseCode(buffer *bytes.Buffer, id string) *CodeChunk {
+func LoadCode(buffer *bytes.Buffer, id string) *CodeChunk {
 	chunk := &CodeChunk{}
-	chunk.Id = id
-	chunk.Length = binary.BigEndian.Uint32(buffer.Next(4))
-	chunk.Version = binary.BigEndian.Uint32(buffer.Next(4))
-	chunk.MaxOpcode = binary.BigEndian.Uint32(buffer.Next(4))
-	chunk.LabelCount = binary.BigEndian.Uint32(buffer.Next(4))
-	chunk.FunCount = binary.BigEndian.Uint32(buffer.Next(4))
+	chunk.load(buffer, id)
 
-	fmt.Println(buffer.Next(1))
+	data := bytes.NewBuffer(chunk.Data)
+	chunk.Version = binary.BigEndian.Uint32(data.Next(4))
+	chunk.MaxOpcode = binary.BigEndian.Uint32(data.Next(4))
+	chunk.LabelCount = binary.BigEndian.Uint32(data.Next(4))
+	chunk.FunCount = binary.BigEndian.Uint32(data.Next(4))
+
+	// TODO
+	//opcodes := []string{}
+	/*
+		for i := uint32(0); i < chunk.FunCount; i++ {
+			fmt.Println(buffer.Next(1))
+			return chunk
+		}
+	*/
+	return chunk
+}
+
+type StrTChunk struct {
+	Chunk
+}
+
+func LoadStrT(buffer *bytes.Buffer, id string) *StrTChunk {
+	chunk := &StrTChunk{}
+	chunk.load(buffer, id)
+
+	// TODO
+	return chunk
+}
+
+type ImpTChunk struct {
+	Chunk
+}
+
+func LoadImpT(buffer *bytes.Buffer, id string) *ImpTChunk {
+	chunk := &ImpTChunk{}
+	chunk.load(buffer, id)
+
+	// TODO
+	return chunk
+}
+
+type ExpTChunk struct {
+	Chunk
+}
+
+func LoadExpT(buffer *bytes.Buffer, id string) *ExpTChunk {
+	chunk := &ExpTChunk{}
+	chunk.load(buffer, id)
+
+	// TODO
+	return chunk
+}
+
+type LitTChunk struct {
+	Chunk
+}
+
+func LoadLitT(buffer *bytes.Buffer, id string) *LitTChunk {
+	chunk := &LitTChunk{}
+	chunk.load(buffer, id)
+
+	// TODO
+	return chunk
+}
+
+type LocTChunk struct {
+	Chunk
+}
+
+func LoadLocT(buffer *bytes.Buffer, id string) *LocTChunk {
+	chunk := &LocTChunk{}
+	chunk.load(buffer, id)
+
+	// TODO
+	return chunk
+}
+
+type AttrChunk struct {
+	Chunk
+}
+
+func LoadAttr(buffer *bytes.Buffer, id string) *AttrChunk {
+	chunk := &AttrChunk{}
+	chunk.load(buffer, id)
+
+	// TODO
+	return chunk
+}
+
+type CInfChunk struct {
+	Chunk
+}
+
+func LoadCInf(buffer *bytes.Buffer, id string) *CInfChunk {
+	chunk := &CInfChunk{}
+	chunk.load(buffer, id)
+
+	// TODO
+	return chunk
+}
+
+type DbgiChunk struct {
+	Chunk
+}
+
+func LoadDbgi(buffer *bytes.Buffer, id string) *DbgiChunk {
+	chunk := &DbgiChunk{}
+	chunk.load(buffer, id)
+
+	// TODO
+	return chunk
+}
+
+type DocsChunk struct {
+	Chunk
+}
+
+func LoadDocs(buffer *bytes.Buffer, id string) *DocsChunk {
+	chunk := &DocsChunk{}
+	chunk.load(buffer, id)
+
+	// TODO
+	return chunk
+}
+
+type ExDpChunk struct {
+	Chunk
+}
+
+func LoadExDp(buffer *bytes.Buffer, id string) *ExDpChunk {
+	chunk := &ExDpChunk{}
+	chunk.load(buffer, id)
+
+	// TODO
+	return chunk
+}
+
+type LineChunk struct {
+	Chunk
+}
+
+func LoadLine(buffer *bytes.Buffer, id string) *LineChunk {
+	chunk := &LineChunk{}
+	chunk.load(buffer, id)
+
+	// TODO
 	return chunk
 }
 
@@ -90,9 +242,20 @@ type BeamData struct {
 	Type      string // 'BEAM'
 	AtomChunk *AtomChunk
 	CodeChunk *CodeChunk
+	StrTChunk *StrTChunk
+	ImpTChunk *ImpTChunk
+	ExpTChunk *ExpTChunk
+	LitTChunk *LitTChunk
+	LocTChunk *LocTChunk
+	AttrChunk *AttrChunk
+	CInfChunk *CInfChunk
+	DbgiChunk *DbgiChunk
+	DocsChunk *DocsChunk
+	ExDpChunk *ExDpChunk
+	LineChunk *LineChunk
 }
 
-func ParseBeamFile(beamPath string) (*BeamData, error) {
+func LoadBeamFile(beamPath string) (*BeamData, error) {
 
 	file, err := os.Open(beamPath)
 	if err != nil {
@@ -113,15 +276,38 @@ func ParseBeamFile(beamPath string) (*BeamData, error) {
 	data.Type = string(buffer.Next(4))
 
 	for buffer.Len() > 0 {
-		id := string(buffer.Next(4))
+		bs := buffer.Next(4)
+		id := string(bs)
+		fmt.Println(id)
 		switch id {
 		case idAtom, idAtU8:
-			data.AtomChunk = ParseAtom(buffer, id)
+			data.AtomChunk = LoadAtom(buffer, id)
 		case idCode:
-			data.CodeChunk = ParseCode(buffer, id)
+			data.CodeChunk = LoadCode(buffer, id)
+		case idStrT:
+			data.StrTChunk = LoadStrT(buffer, id)
+		case idImpT:
+			data.ImpTChunk = LoadImpT(buffer, id)
+		case idExpT:
+			data.ExpTChunk = LoadExpT(buffer, id)
+		case idLitT:
+			data.LitTChunk = LoadLitT(buffer, id)
+		case idLocT:
+			data.LocTChunk = LoadLocT(buffer, id)
+		case idAttr:
+			data.AttrChunk = LoadAttr(buffer, id)
+		case idCInf:
+			data.CInfChunk = LoadCInf(buffer, id)
+		case idDbgi:
+			data.DbgiChunk = LoadDbgi(buffer, id)
+		case idDocs:
+			data.DocsChunk = LoadDocs(buffer, id)
+		case idExDp:
+			data.ExDpChunk = LoadExDp(buffer, id)
+		case idLine:
+			data.LineChunk = LoadLine(buffer, id)
 		default:
-			//fmt.Println("id:", id)
-			break
+			return nil, errors.New(fmt.Sprintf("unknown id: %s %v", id, bs))
 		}
 	}
 
@@ -131,6 +317,10 @@ func ParseBeamFile(beamPath string) (*BeamData, error) {
 func main() {
 	beamPath := os.Args[1]
 
-	data, _ := ParseBeamFile(beamPath)
+	data, err := LoadBeamFile(beamPath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	pp.Println(data)
 }
